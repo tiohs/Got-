47828cdcb7afc48d4aca453ce5528be9cd7c6dca
HamiltonMassango <hamiltonsilva551@gmail.com>
2019-09-20 22:14:07 -0300

    Validação de pages cadastro validation feito ^_^

--- src/controllers/cadastro.js
+++ src/controllers/cadastro.js
@@ -1,3 +1,17 @@
 module.exports.cadastro = function (app, req, res) {
-    res.render('cadastro');
+    res.render('cadastro', {dados : {}, dadosFor: {}});
+}
+module.exports.cadastrar = function (app, req, res) {
+    var dadosFor = req.body;
+    req.assert('nome', 'Nome não pode ser vazio ').notEmpty();
+    req.assert('usuario', 'Usuário não pode ser vazio ').notEmpty();
+    req.assert('senha', 'Senha não pode ser vazio ').notEmpty();
+    req.assert('casa', 'Casa não pode ser vazio ').notEmpty();
+    var erros = req.validationErrors();
+    if(erros){
+        res.render('cadastro', {dados : erros, dadosFor : dadosFor});
+        return ;
+    }
+    res.send('Podemos cadrastrar ');
+    console.log(dadosFor);
 }
--- src/routes/cadastro.js
+++ src/routes/cadastro.js
@@ -1,3 +1,4 @@
 module.exports = function (app){
     app.get('/cadastro', (req, res) => app.src.controllers.cadastro.cadastro(app, req, res));
+    app.post('/cadastrar', (req, res) => app.src.controllers.cadastro.cadastrar(app, req, res));
 }
